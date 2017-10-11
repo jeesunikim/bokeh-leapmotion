db11aea195eb6842f47692243c3f15deced9b6c3
Jeesun Kim <jeesunkim@JEESUNs-MBP.fios-router.home>
2017-10-10 21:57:01 -0400

    feature/9/HandRotation > upon hand rotation, change background implemented

---
+++ src/components/Canvas.js
@@ -0,0 +1,35 @@
+import { ParseToNum, getRandom } from '../utils';
+import { addClass, concatData } from '../utils';
+
+const body = document.body;
+
+export default class Canvas {
+	constructor(canvas, context) {
+		this.canvas = canvas;
+		this.context = context;
+
+		this.changeToScene = this.changeToScene.bind(this);
+	}
+
+	rotateToChange(rotation) {
+
+		let defaultScene = rotation < .8 && rotation > 0 ? true : false, // less than .5
+			nextScene = rotation < -.5,
+			previousScene = rotation > .8;
+
+		if(defaultScene) {
+			this.changeToScene("defaultScene");	
+		}
+		if(nextScene){
+			this.changeToScene("nextScene")
+		}
+		if(previousScene){
+			this.changeToScene("previousScene")
+		}
+	}
+
+	changeToScene(scene) {
+		body.className="";
+		addClass(body, scene);
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import { addClass } from './utils';
 import * as LeapMotionDetect from "./components/LeapMotionDetect"; 
 
 document.addEventListener("DOMContentLoaded", () => {
+	addClass(document.body, "defaultScene");
 	console.log("Bokeh DOM has been loaded");
 });
